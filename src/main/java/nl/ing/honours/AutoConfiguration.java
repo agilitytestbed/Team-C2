3c29a022ef17919c1b88952389fa14d8b81950aa
CPlentinger <Casperplentinger@gmail.com>
2018-02-05 17:34:18 +0100

    Configure H2 database
    Added database config with encrypted password.

--- src/main/java/nl/ing/honours/AutoConfiguration.java
+++ src/main/java/nl/ing/honours/AutoConfiguration.java
@@ -1,10 +1,12 @@
 package nl.ing.honours;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
+@EnableEncryptableProperties
 public class AutoConfiguration {
 
 }
--- src/main/java/nl/ing/honours/DPAApplication.java
+++ src/main/java/nl/ing/honours/DPAApplication.java
@@ -1,5 +1,6 @@
 package nl.ing.honours;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
